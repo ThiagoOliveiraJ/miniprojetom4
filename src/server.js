9a2a3e70a8bd4454ebcb0b6b8f1539e5dd36718e
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-09-23 18:29:43 -0300

    fix: config cors get

--- src/server.js
+++ src/server.js
@@ -2,15 +2,16 @@ import express from "express"
 import cors from "cors";
 import { tipsRouter } from "./routes/social_wellness.routes.js"
 
+const app = express();
+const port = 3000;
+
 const corsOptions = {
     origin: 'https://mente-saudavel.vercel.app',
     methods: "GET,POST,PUT,DELETE",
   };
   
-  app.use(cors(corsOptions));
-  
-const app = express();
-const port = 3000;
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 
